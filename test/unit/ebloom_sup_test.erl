7fb4324754b43b1e27e98665df48ba5f2f5d2858
Johannes Huning <mail@johanneshuning.com>
2012-11-02 12:45:55 +0100

    Commit the main sup's test

---
+++ test/unit/ebloom_sup_test.erl
@@ -0,0 +1,7 @@
+-module (ebloom_sup_test).
+-compile (export_all).
+-include_lib ("etest/include/etest.hrl").
+
+test_main_sup_spec() ->
+    % When requesting the supervisor spec, expect it to be empty.
+    ?assert_equal(et_sup:spec([]), ebloomd_sup:init([])).
